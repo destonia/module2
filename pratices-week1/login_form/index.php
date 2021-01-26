76b13f73272871bc6c883563e3d6aabae503c2e1
thanh <lanhthanh.dktd@gmail.com>
2021-01-26 20:58:24 +0700

    module2-week2-practices

--- pratices-week1/login_form/index.php
+++ pratices-week1/login_form/index.php
@@ -12,7 +12,7 @@
     }
 </style>
 <body>
-<form method="get" /*method="post"*/>
+<form method="POST">
     <div class="login">
         <h2>Login</h2>
         <input type="text" name="username" size="30"  placeholder="username" />
@@ -20,17 +20,19 @@
         <input type="submit" value="Sign in"/>
     </div>
 </form>
-</body>
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    if ($username === "admin" && $password === "admin") {
-        echo "<h2>Welcome <span style='color:red'>" .$username. "</span> to website</h2>";
-    } else{
-        echo "<h2><span style='color:red'>Login Error</span></h2>";
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $userName=$_POST["username"];
+    $password=$_POST["password"];
+    if(($userName!=="admin")||($password!=="admin")){
+        echo ("Wrong user name or password") ;
+    }
+    else if($userName==='admin'&&$password=="admin"){
+        header("location: welcome.php");
     }
 }
+
 ?>
+</body>
+
 </html>
